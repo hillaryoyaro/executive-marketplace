3ce8659a3aa254b9583cfd6c7976823d9dfdd788
oyaro <hillaryoyaroh@gmail.com>
2025-08-03 09:44:26 +0300

    update of logo

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 import logo from "./orebiLogo.png";
 import logoLight from "./logoLight.png";
+import executivesLogo from "./executivesLogo.png";
 import bannerImgOne from "./banner/bannerImgOne.webp";
 import bannerImgTwo from "./banner/bannerImgTwo.webp";
 import bannerImgThree from "./banner/bannerImgThree.webp";
@@ -34,6 +35,7 @@ import emptyCart from "../images/emptyCart.png";
 export {
   logo,
   logoLight,
+  executivesLogo,
   bannerImgOne,
   bannerImgTwo,
   bannerImgThree,
